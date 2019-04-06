24a1145a8b74fd2a9cc6f961c83e63090dc2cbbe
Zack Rock <zacharyrock@hotmail.com>
2019-04-05 20:35:46 -0700

    TestEnemy

--- Assets/_Game/Scripts/Enemy.cs
+++ Assets/_Game/Scripts/Enemy.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    int row;
-    int column;
+    public int row = 1;
+    public int column = 3;
 
-    IEnumerable Start()
+    IEnumerator Start()
     {
         yield return null;
         yield return null;
@@ -25,27 +25,54 @@ public class Enemy : MonoBehaviour
             int nextRow = -1;
             int nextColumn = -1;
             int tries = 0;
-            while(nextRow != -1 && nextColumn != -1 && tries < 20)
+            while(nextRow == -1 && nextColumn == -1 && tries < 20)
             {
                 int horizontalOrVertical = Random.Range(0, 2);
                 if(horizontalOrVertical == 0)
                 {
                     int leftOrRight = Random.Range(0, 2);
-                    if(leftOrRight == 0)
+                    if(leftOrRight == 0) //Left
                     {
-                        column--;
-                        if (column >= 0)
+                        if (column - 1 >= 3)
                         {
-                            nextColumn = 0;
+                            nextColumn = column - 1;
+                            nextRow = row;
+                        }
+                    }
+                    else //Right
+                    {
+                        if (column + 1 < GridMap.WIDTH)
+                        {
+                            nextColumn = column + 1;
+                            nextRow = row;
                         }
                     }
                 }
                 else
                 {
                     int upOrDown = Random.Range(0, 2);
+                    if (upOrDown == 0) //Up
+                    {
+                        if (row - 1 >= 0)
+                        {
+                            nextColumn = column;
+                            nextRow = row - 1;
+                        }
+                    }
+                    else //Down
+                    {
+                        if (row + 1 < GridMap.HEIGHT)
+                        {
+                            nextColumn = column;
+                            nextRow = row + 1;
+                        }
+                    }
                 }
                 tries++;
             }
+            row = nextRow;
+            column = nextColumn;
+            transform.position = MapManager.instance.currentMap.GetTilePosition(row, column);
         }
     }
 }
